b15304a5b68136e38a1c415659a95afc0d036af9
SergKhachikyan <159169375+SergKhachikyan@users.noreply.github.com>
2025-08-08 17:50:11 +0400

    Delete CodeWars/8_kyu directory

--- CodeWars/8_kyu/The_falling_speed_of_petals.py
+++
@@ -1,6 +0,0 @@
-def sakura_fall(y):
-    if y <= 0:
-        return 0
-    return 400 / y
-
-print(sakura_fall(5))
--- CodeWars/8_kyu/Was_the_package_received_before_it_was_sent.py
+++
@@ -1,7 +0,0 @@
-def was_package_received_yesterday(tz_from, tz_to, start, duration):
-    utc_departure = start - tz_from
-    utc_arrival = utc_departure + duration
-
-    local_arrival = utc_arrival + tz_to
-
-    return local_arrival < 0
